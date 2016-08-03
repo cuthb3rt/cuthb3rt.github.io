d2fb4561ddc14604864cf010b6c5c71382332551
Andy <andycuth@gmail.com>
2016-08-03 20:02:41 +0100

    Tweaked make file

--- make.bat
+++ make.bat
@@ -2,16 +2,10 @@
 
 REM Command file for Sphinx documentation
 
-if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
-)
+set SPHINXBUILD=sphinx-build
 set BUILDDIR=../cuthb3rt.github.io
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% .
 set I18NSPHINXOPTS=%SPHINXOPTS% .
-if NOT "%PAPER%" == "" (
-	set ALLSPHINXOPTS=-D latex_paper_size=%PAPER% %ALLSPHINXOPTS%
-	set I18NSPHINXOPTS=-D latex_paper_size=%PAPER% %I18NSPHINXOPTS%
-)
 
 %SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%
 if errorlevel 1 exit /b 1
